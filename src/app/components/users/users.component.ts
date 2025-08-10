c6347eb146bb69c1bb71688207eb83fcb28f09f4
dany <antonydany10@hotmail.com>
2025-08-10 15:45:59 -0600

    inputs y outputs para comunicanión con el padre

--- src/app/components/users/users.component.ts
+++ src/app/components/users/users.component.ts
@@ -1,46 +1,26 @@
-import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { UserService, User } from '../../services/user.service';
+import { User } from '../../services/user.service';
 
 @Component({
   selector: 'app-users',
   standalone: true,
-  imports: [FormsModule, CommonModule],
+  imports: [CommonModule],
   templateUrl: './users.component.html'
 })
+
+
 export class UsersComponent {
-  users: User[] = [];
-  editing = false;
-  currentUser: User = { id: 0, name: '', email: '' };
+  @Input() users: User[] = [];
+  @Output() edit = new EventEmitter<User>();
+  @Output() delete = new EventEmitter<number>();
 
-  constructor(private userService: UserService) {
-    this.loadUsers();
+  onEdit(user: User) {
+    this.edit.emit(user);
   }
 
-  loadUsers() {
-    this.users = this.userService.getUsers();
-  }
-
-  saveUser() {
-    if (this.editing) {
-      this.userService.updateUser(this.currentUser);
-    } else {
-      this.userService.addUser(this.currentUser);
-    }
-    this.currentUser = { id: 0, name: '', email: '' };
-    this.editing = false;
-    this.loadUsers();
-  }
-
-  editUser(user: User) {
-    this.currentUser = { ...user };
-    this.editing = true;
-  }
-
-  deleteUser(id: number) {
-    this.userService.deleteUser(id);
-    this.loadUsers();
+  onDelete(id: number) {
+    this.delete.emit(id);
   }
 }
 
